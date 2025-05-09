aa77b9e3588d69d04a9502c26959135ca2d4b797
Tafita Mathieu <hei.tafita.2@gmail.com>
2025-05-09 17:11:15 +0300

    Update url.js

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,8 +1,5 @@
 export const API_URL =
   window.location.hostname === "127.0.0.1"
     ? "http://localhost:3000/api"
-<<<<<<< HEAD
-    : "https://driftype-backend-production.up.railway.app/api";
-=======
     : "https://driftype-typing-game.onrender.com/api";
->>>>>>> parent of 39ce184 (Update url.js)
+
