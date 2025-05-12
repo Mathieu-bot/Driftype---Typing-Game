a514f857ede6e9d6f3a976e678170e5beac78428
Tafita Mathieu <hei.tafita.2@gmail.com>
2025-05-12 23:04:26 +0300

    Update url.js

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,4 +1,4 @@
 export const API_URL =
   window.location.hostname === "127.0.0.1"
     ? "http://localhost:3000/api"
-    : "driftype-backend-production-35e2.up.railway.app/api";
+    : "driftype-backend.railway.internal/api";
